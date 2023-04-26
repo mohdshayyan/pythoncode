10e06d7e9a25e314309a75e919ea7850e60a9009
SHAYYAN <127933448+mohdshayyan@users.noreply.github.com>
2023-04-26 23:19:12 +0530

    Update project 2.py

--- project 2.py
+++ project 2.py
@@ -17,8 +17,82 @@ def menu():
     print("(9.) Add   deposit details                                           (10.) View   datails")
     print("(11.) Update   datails                                                  (12.) Delete   datails                     (13.) Exit")
 
+# Define the function to add a new student
+def addEmp():
+    print("*******************ADD NEW EMPLOYEE**************************")
+    eno =input("Enter employee number :")
+    en = input("Enter employee name :")
+    dp = input("Enter department ")
+    sl = input("Enter Salary :")
+    cursor = mydb.cursor()
+    # CREATING A TABLE
+    cursor.execute('CREATE TABLE  EMPLOYEE(enoVARCHAR(255),en VARCHAR(255),dp VARCHAR(255),sl VARCHAR(255))')
+# Inserting Values
+    sql = "INSERT INTO EMPLOYEE(eno,en, dp, sl) VALUES (%s, %s, %s, %s)"
+    val = (eno,en, dp, sl)
+    cursor.execute(sql, val)
+    mydb.commit()
+    print(cursor.rowcount, "record(s) inserted.")
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Call the menu function
+while True:
+    menu()
+    
+    # Get the user's choice
+    choice = input("Enter your choice: ")
+
+    # Execute the selected option
+    if choice == '1':
+        addEmp()
+    else:
+        print("Invalid choice. Please try again.")
+# Disconnecting from the server
+mydb.close()
 
 
 
