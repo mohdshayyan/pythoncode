14ab1eb916abd1d8a3605f50dffbc0b5cf7da31e
SHAYYAN <127933448+mohdshayyan@users.noreply.github.com>
2023-09-21 02:18:53 -0700

    Delete PY.py

--- PY.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-idx=['Physics','Biology','English','Chemistry']
-data={'A':[67,89,90,95],'B':[20,50,60,94],'C':[70,80,90,60],'D':[80,90,59,85]}
-df=pd.DataFrame(data,index=idx)
-print(df)
-df.to_csv('Pd')
